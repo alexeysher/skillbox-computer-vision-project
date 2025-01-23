afa41db9dd92bc60e61644b5d40573c5cb351043
ALEXEY SHERSTOBITOV <107345313+alexeysher@users.noreply.github.com>
2025-01-23 18:31:14 +0200

    Update emotion_recognition.py

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -5,50 +5,49 @@ from keras import models
 from typing import Union
 
 EMOTIONS = {
-    'anger': (-0.41, 0.79),  # гнев, злость
-    'contempt': (-0.57, 0.66),  # презрение
-    'disgust': (-0.67, 0.49),  # отвращение
-    'fear': (-0.12, 0.78),  # страх
-    'happy': (0.9, 0.16),  # веселый
-    'neutral': (0.0, 0.0),  # нейтральный
-    'sad': (-0.82, -0.4),  # грусть
-    'surprise': (0.37, 0.91),  # удивление
-    'uncertain': (-0.5, 0.0),  # неуверенность
+    'anger': (-0.41, 0.79), # anger, rage
+    'contempt': (-0.57, 0.66), # contempt
+    'disgust': (-0.67, 0.49), # disgust
+    'fear': (-0.12, 0.78), # fear
+    'happy': (0.9, 0.16), # cheerful
+    'neutral': (0.0, 0.0), # neutral
+    'sad': (-0.82, -0.4), # sadness
+    'surprise': (0.37, 0.91), # surprise
+    'uncertain': (-0.5, 0.0), # uncertainty
 }
 
 EmotionPrediction = namedtuple('EmotionPrediction', ['emotion', 'probability'])
 ValenceArousalPrediction = namedtuple('ValenceArousalPrediction', ['emotion', 'error', 'valence', 'arousal'])
 
-
 class FaceEmotionRecognitionNet:
 
     def __init__(self, model_path: str, emotions=None):
         """
-        file_path: путь к файлу сохраненной модели.
-        emotions: предсказываемые эмоции.
+        file_path: path to the saved model file.
+        emotions: predicted emotions.
         """
-        # Загружаем модель
+        # Load the model
         if emotions is None:
             emotions = EMOTIONS
         self.__model = models.load_model(filepath=model_path, compile=False)
         self.__emotions = emotions
 
     def predict(self, face_image: np.array) -> Union[EmotionPrediction, ValenceArousalPrediction]:
-        """Предсказание эмоции человека по изображению его лица.
-
-        Аргументы:
-        - face_image: изображение лица человека.
+        """Predict a person's emotion based on their face.
+        
+        Arguments:
+        - face_image: image of a person's face.
         """
         image = Image.fromarray(face_image)
         size = max(image.width, image.height)
-        # Делаем изображение квадратным
+        # Make the image square
         padded_image = ImageOps.pad(image, (size, size))
-        # Подгоняем размер изображения
+        # Adjust the image size
         resized_image = padded_image.resize(self.__model.input_shape[1:3])
-        # Получаем предсказание
+        # Get the prediction
         tensor = np.asarray(resized_image)[None, ...]
         predicts = self.__model.predict(tensor, verbose=0)[0]
-        # Готовим результирующие данные
+        # Prepare the resulting data
         if isinstance(self.__emotions, (list, tuple)):
             probability = predicts.max()
             label = predicts.argmax()
