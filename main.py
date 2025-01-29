23314c57ede54033f7d9cf519eeccca385d98ac8
alexeysher <asherstobitov@mail.ru>
2025-01-29 21:37:37 +0200

    Add requirements.txt

--- main.py
+++ main.py
@@ -15,24 +15,91 @@ def intro():
     st.markdown(author, unsafe_allow_html=True)
 
 
+def concept():
+    st.markdown('<h1 style="text-align:center; color:blue">Model types</h1>', unsafe_allow_html=True)
+    c1, c2 = st.columns(2, gap='medium')
+    with c1:
+        st.markdown('<h2 style="text-align:center">1st type</h2>', unsafe_allow_html=True)
+        st.markdown('### Predicts the probability of human emotion based on facial expression')
+    with c2:
+        st.markdown('<h2 style="text-align:center">2nd type</h2>', unsafe_allow_html=True)
+        st.markdown('### Predicts levels of valence and intensity (arousal) of human emotion based on facial expression')
+    c1, c2 = st.columns(2, gap='medium')
+    st.markdown('<h3 style="text-align:center; color:darkorange">Learning</h3>', unsafe_allow_html=True)
+    c1, c2 = st.columns(2, gap='medium')
+    with c1:
+        st.image('https://user-images.githubusercontent.com/107345313/'
+                 '200283993-4ec70b6c-9da7-4355-891a-564332559041.svg',
+                 use_container_width=True)
+        st.markdown('')
+    with c2:
+        st.image('https://user-images.githubusercontent.com/107345313/'
+                 '200284152-5faabe63-43ab-4593-828b-2684ef35b70f.svg',
+                 use_container_width=True)
+        st.markdown('')
+    c1, c2 = st.columns(2, gap='medium')
+    with c1:
+        st.markdown('''
+    ```python
+    EMOTIONS = (
+        'anger', # anger, rage
+        'contempt', # contempt
+        'disgust', # disgust
+        'fear', # fear
+        'happy', # happiness
+        'neutral', # neutral
+        'sad', # sadness
+        'surprise', # surprise
+        'uncertain', # uncertain
+    )
+    ```                ''')
+    with c2:
+        st.markdown('''
+    ```python
+    EMOTIONS = {
+        'anger': (-0.41, 0.79), # anger, rage
+        'contempt': (-0.57, 0.66), # contempt
+        'disgust': (-0.67, 0.49), # disgust
+        'fear': (-0.12, 0.78), # fear
+        'happy': (0.9, 0.16), # happiness
+        'neutral': (0.0, 0.0), # neutral
+        'sad': (-0.82, -0.4), # sadness
+        'surprise': (0.37, 0.91), # surprise
+        'uncertain': (-0.5, 0.0), # uncertain
+    }
+    ```                        ''')
+    st.markdown('<h3 style="text-align:center; color:darkorange">Inference</h3>', unsafe_allow_html=True)
+    c1, c2 = st.columns(2, gap='medium')
+    with c1:
+        st.image('https://user-images.githubusercontent.com/107345313/'
+                 '200292114-4c6a4e79-a151-463c-a3d9-e22bfa526efa.svg',
+                 use_container_width=True)
+        st.markdown('')
+    with c2:
+        st.image('https://user-images.githubusercontent.com/107345313/'
+                 '200292181-72ec54e2-f367-4c06-854f-ce541722d108.svg',
+                 use_container_width=True)
+        st.markdown('')
+
+
 def learning():
-    st.markdown('<h1 style="text-align:center; color:blue">Процесс создания моделей</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 style="text-align:center; color:blue">Model building</h1>', unsafe_allow_html=True)
     c1, c2, c3 = st.columns([25, 30, 45], gap='medium')
     c1.markdown('<h2 style="text-align:center">Pipeline</h2>', unsafe_allow_html=True)
     c2.markdown('<h2 style="text-align:center">Stage</h2>', unsafe_allow_html=True)
     c3.markdown('<h2 style="text-align:center">Tasks</h2>', unsafe_allow_html=True)
     c1, c2, c3 = st.columns([25, 30, 45], gap='medium')
     c1.markdown('---')
-    c1.markdown('#### 1. Obtaining information about base models in [Keras Applications]'
+    c1.markdown('#### 1. Collecting information about base models in [Keras Applications]'
                 '(https://keras.io/api/applications/)')
     c2.markdown('---')
-    c2.markdown('#### 1.1. Obtaining information about sizes of input images and '
+    c2.markdown('#### 1.1. Collecting information about sizes of input images and '
                 'feature vectors')
     c3.markdown('---')
     text = '''
     <ul>
-    <li><span style="fontSize:24px">Obtaining information about the optimal input image sizes for each base model</span></li>
-    <li><span style="fontSize:24px">Obtaining information about the sizes of feature vectors at the output of the last pooling layer of each "base model"</span></li>
+    <li><span style="fontSize:24px">Collecting information about the optimal input image sizes for each base model</span></li>
+    <li><span style="fontSize:24px">Collecting information about the sizes of feature vectors at the output of the last pooling layer of each "base model"</span></li>
     </ul>
     '''
     c3.markdown(text, unsafe_allow_html=True)
@@ -40,12 +107,12 @@ def learning():
     c2.markdown('#### 1.2. Inference time measuring')
     text = '''
     <ul>
-    <li><span style="fontSize:24px">Estimation of the inference time of a model consisting of a base model combined with the "heaviest" upper model>/span></li>
+    <li><span style="fontSize:24px">Estimation of the inference time of a model consisting of a base model combined with the "heaviest" upper model</span></li>
     </ul>
     '''
     c3.markdown(text, unsafe_allow_html=True)
     _, c2, c3 = st.columns([25, 30, 45], gap='medium')
-    c2.markdown('#### 1.3. Selecting base model')
+    c2.markdown('#### 1.3. Base model selection')
     text = '''
     <ul>
     <li><span style="fontSize:24px">Selecting a base model with the best combination of performance and accuracy, shown on the ImageNet validation dataset</li>
@@ -90,19 +157,19 @@ def learning():
     c1.markdown('---')
     c1.markdown('#### 3. Model building')
     c2.markdown('---')
-    c2.markdown('#### 3.1. Feature exctraction from the train dataset')
+    c2.markdown('#### 3.1. Feature extraction from the train dataset')
     c3.markdown('---')
     text = '''
     <ul>
-    <li><span style="fontSize:24px">Obtaining a dataset of feature vectors from the train dataset faces for use in selecting the best model on top</span></li>
+    <li><span style="fontSize:24px">Collecting a dataset of feature vectors from the train dataset faces for use in selecting the best model on top</span></li>
     </ul>
     '''
     c3.markdown(text, unsafe_allow_html=True)
     _, c2, c3 = st.columns([25, 30, 45], gap='medium')
-    c2.markdown('#### 3.2. Feature exctraction from the test dataset')
+    c2.markdown('#### 3.2. Feature extraction from the test dataset')
     text = '''
     <ul>
-   <li><span style="fontSize:24px">Obtaining a dataset of feature vectors from the test dataset faces for use in selecting the best model on top</span></li>
+   <li><span style="fontSize:24px">Collecting a dataset of feature vectors from the test dataset faces for use in selecting the best model on top</span></li>
      </ul>
     '''
     c3.markdown(text, unsafe_allow_html=True)
@@ -118,8 +185,8 @@ def learning():
     c2.markdown('#### 3.4. Model on top learning')
     text = '''
     <ul>
-    <li><span style="fontSize:24px">Снижение вероятности возникновения слишком большой величины градиента при обучении 
-    модели целиком, которое может привести к разрушению базовой модели</span></li>
+    <li><span style="fontSize:24px">Reducing the likelihood of too large a gradient occurring 
+    when training an entire model, which could lead to the destruction of the underlying model</span></li>
     </ul>
     '''
     c3.markdown(text, unsafe_allow_html=True)
@@ -135,7 +202,7 @@ def learning():
     c2.markdown('#### 3.6. Model testing')
     text = '''
     <ul>
-    <li><span style="fontSize:24px">ОTesting is performed on an image from the built-in camera of the local computer
+    <li><span style="fontSize:24px">Testing is performed on an image from the built-in camera of the local computer
     </span></li>
     </ul>
     '''
